8039a5a7ad797c883ef72a3a3577f4fc3c505073
Srikanth <srikanth@srikanth-Lenovo-Z580.(none)>
2013-11-14 22:47:41 -0600

    Intro to Scala assignments

---
+++ streams/src/main/scala/streams/Solver.scala
@@ -0,0 +1,121 @@
+package streams
+
+import common._
+
+/**
+ * This component implements the solver for the Bloxorz game
+ */
+trait Solver extends GameDef {
+
+  /**
+   * Returns `true` if the block `b` is at the final position
+   */
+  def done(b: Block): Boolean = b.b1.x == goal.x && b.b1.y == goal.y && b.b2.x == goal.x && b.b2.y == goal.y
+
+  /**
+   * This function takes two arguments: the current block `b` and
+   * a list of moves `history` that was required to reach the
+   * position of `b`.
+   *
+   * The `head` element of the `history` list is the latest move
+   * that was executed, i.e. the last move that was performed for
+   * the block to end up at position `b`.
+   *
+   * The function returns a stream of pairs: the first element of
+   * the each pair is a neighboring block, and the second element
+   * is the augmented history of moves required to reach this block.
+   *
+   * It should only return valid neighbors, i.e. block positions
+   * that are inside the terrain.
+   */
+  def neighborsWithHistory(b: Block, history: List[Move]): List[(Block, List[Move])] = {
+    def neighborsWithHistory(b: Block, history: List[Move], ngbrs: List[(Block, Move)]): List[(Block, List[Move])] = {
+      if (ngbrs == Nil) Nil
+      else {
+        val head = ngbrs.head
+        (head._1, head._2 :: history) :: neighborsWithHistory(b, history, ngbrs.tail)
+      }
+    }
+    neighborsWithHistory(b, history, b.legalNeighbors)
+  }
+
+  /**
+   * This function returns the list of neighbors without the block
+   * positions that have already been explored. We will use it to
+   * make sure that we don't explore circular paths.
+   */
+  def newNeighborsOnly(neighbors: List[(Block, List[Move])],
+    explored: Set[Block]): List[(Block, List[Move])] = {
+    neighbors.filter(t => !explored.contains(t._1))
+  }
+
+  /**
+   * The function `from` returns the stream of all possible paths
+   * that can be followed, starting at the `head` of the `initial`
+   * stream.
+   *
+   * The blocks in the stream `initial` are sorted by ascending path
+   * length: the block positions with the shortest paths (length of
+   * move list) are at the head of the stream.
+   *
+   * The parameter `explored` is a set of block positions that have
+   * been visited before, on the path to any of the blocks in the
+   * stream `initial`. When search reaches a block that has already
+   * been explored before, that position should not be included a
+   * second time to avoid cycles.
+   *
+   * The resulting stream should be sorted by ascending path length,
+   * i.e. the block positions that can be reached with the fewest
+   * amount of moves should appear first in the stream.
+   *
+   * Note: the solution should not look at or compare the lengths
+   * of different paths - the implementation should naturally
+   * construct the correctly sorted stream.
+   */
+  def from(initial: List[(Block, List[Move])],
+    explored: Set[Block]): Stream[(Block, List[Move])] = initial match {
+
+    case Nil => Stream()
+    case x :: xs => {
+      val t = explored
+      val n = for {
+        node <- initial
+        ngbrs <- newNeighborsOnly(neighborsWithHistory(node._1, node._2), explored)
+      } yield (ngbrs._1, ngbrs._2)
+
+      val e = for {
+        i <- n
+      } yield i._1
+      (n ++ from(n, explored.union(e.toSet))).toStream
+    }
+  }
+
+  /**
+   * The stream of all paths that begin at the starting block.
+   */
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(List((startBlock, List())), Set())
+
+  /**
+   * Returns a stream of all possible pairs of the goal block along
+   * with the history how it was reached.
+   */
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+    for {
+      p <- pathsFromStart
+      if done(p._1)
+    } yield p
+  }
+
+  /**
+   * The (or one of the) shortest sequence(s) of moves to reach the
+   * goal. If the goal cannot be reached, the empty list is returned.
+   *
+   * Note: the `head` element of the returned list should represent
+   * the first move that the player should perform from the starting
+   * position.
+   */
+  lazy val solution: List[Move] = pathsToGoal match {
+    case Stream() => List()
+    case x #:: xs => x._2.reverse
+  }
+}
